af1a949ea626438a0eab67f9936863d237d2346a
Alfonso Soldado Caro <32943650+AlfonsoSoldado@users.noreply.github.com>
2017-12-04 14:50:19 +0100

    Tiles de Config y Controllers

---
+++ Workspace-1.0/Acme-Explorer/src/main/java/controllers/ApplicationExplorerController.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import services.ApplicationService;
+import services.ExplorerService;
+import domain.Application;
+
+@Controller
+@RequestMapping("/application/explorer")
+public class ApplicationExplorerController extends AbstractController{
+
+	
+	//Services -------------------------------------------------------------
+	
+	@Autowired
+	private ApplicationService applicationService;
+	
+	@Autowired
+	private ExplorerService explorerService;
+	
+	//Constructors ---------------------------------------------------------
+	
+	public ApplicationExplorerController(){
+		super();
+	}
+	
+	//Listing --------------------------------------------------------------
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView list(){
+		ModelAndView result;
+		Collection<Application> applications;
+		
+		applications = applicationService.findAll();
+		
+		result = new ModelAndView("application/explorer/list");
+		result.addObject("applications", applications);
+		result.addObject("requestURI", "application/explorer/list.do");
+		
+		return result;
+	}
+}

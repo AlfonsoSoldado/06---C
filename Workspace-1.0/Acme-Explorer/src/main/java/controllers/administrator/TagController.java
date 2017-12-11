bb98f55472c793415c5a787a4b63091828816f31
Mario <codec993@gmail.com>
2017-12-11 17:28:55 +0100

    Tag controller

--- Workspace-1.0/Acme-Explorer/src/main/java/controllers/administrator/TagController.java
+++ Workspace-1.0/Acme-Explorer/src/main/java/controllers/administrator/TagController.java
@@ -4,14 +4,18 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import controllers.AbstractController;
 
 import services.TagService;
+import services.ValueService;
 import domain.Tag;
+import domain.Value;
 
 @Controller
 @RequestMapping("/tag")
@@ -20,6 +24,9 @@ public class TagController extends AbstractController {
 
 	@Autowired
 	private TagService tagService;
+	
+	@Autowired
+	private ValueService valueService;
 
 	// Constructors ---------------------------------------------------------
 
@@ -42,4 +49,42 @@ public class TagController extends AbstractController {
 		
 		return result;
 	}
+	
+	// Editing ---------------------------------------------------------------
+	
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public ModelAndView edit(@RequestParam final int tagId) {
+		ModelAndView result;
+		Tag tag;
+
+		tag = tagService.findOne(tagId);
+		Assert.notNull(tag);
+		result = this.createEditModelAndView(tag);
+
+		return result;
+	}
+	
+	
+	// Ancillary methods --------------------------------------------------
+	
+	protected ModelAndView createEditModelAndView(final Tag tag) {
+		ModelAndView result;
+
+		result = this.createEditModelAndView(tag, null);
+
+		return result;
+	}
+	
+	protected ModelAndView createEditModelAndView(final Tag tag,
+			final String message) {
+		ModelAndView result;
+		final Collection<Value> value;
+		value = this.valueService.findAll();
+		result = new ModelAndView("tag/administrator/edit");
+		result.addObject("value", value);
+		result.addObject("tag", tag);
+		result.addObject("message", message);
+		return result;
+	}
+	
 }

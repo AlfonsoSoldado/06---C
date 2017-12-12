8e8234edcf38a3dea767c0c256f065c25b184502
danirguez <32953186+danirguez@users.noreply.github.com>
2017-12-12 13:25:22 +0100

    Administrator

---
+++ Workspace-1.0/Acme-Explorer/src/main/java/controllers/administrator/ActorAdministratorController.java
@@ -0,0 +1,82 @@
+package controllers.administrator;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import services.AdministratorService;
+import controllers.AbstractController;
+import domain.Administrator;
+
+@Controller
+@RequestMapping("/actor/administrator")
+public class ActorAdministratorController extends AbstractController {
+
+	// Services -------------------------------------------------------------
+
+	@Autowired
+	private AdministratorService administratorService;
+
+	// Constructors ---------------------------------------------------------
+
+	public ActorAdministratorController() {
+		super();
+	}
+
+	// Editing ---------------------------------------------------------------
+
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public ModelAndView edit() {
+		ModelAndView result;
+		Administrator administrator;
+
+		administrator = this.administratorService.findByPrincipal();
+		result = this.createEditModelAndView(administrator);
+
+		return result;
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
+	public ModelAndView save(@Valid final Administrator administrator,
+			final BindingResult binding) {
+		ModelAndView res;
+
+//		if (binding.hasErrors())
+//			res = this.createEditModelAndView(administrator, "actor.params.error");
+//		else
+			try {
+				this.administratorService.save(administrator);
+				res = new ModelAndView("redirect:../../");
+			} catch (final Throwable oops) {
+				res = this.createEditModelAndView(administrator, "actor.commit.error");
+			}
+
+		return res;
+	}
+
+	// Ancillary methods --------------------------------------------------
+
+	protected ModelAndView createEditModelAndView(final Administrator administrator) {
+		ModelAndView result;
+
+		result = this.createEditModelAndView(administrator, null);
+
+		return result;
+	}
+
+	protected ModelAndView createEditModelAndView(final Administrator administrator,
+			final String message) {
+		ModelAndView result;
+		result = new ModelAndView("actor/edit");
+		result.addObject("actor", administrator);
+		result.addObject("message", message);
+		result.addObject("requestUri", "actor/administrator/edit.do");
+		return result;
+
+	}
+}

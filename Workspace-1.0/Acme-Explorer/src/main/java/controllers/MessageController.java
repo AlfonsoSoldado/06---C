5b3871c60eef6fcde1e0d7cd07e13d6a0c0e2dbc
danirguez <32953186+danirguez@users.noreply.github.com>
2017-12-13 17:18:04 +0100

    Message

--- Workspace-1.0/Acme-Explorer/src/main/java/controllers/MessageController.java
+++ Workspace-1.0/Acme-Explorer/src/main/java/controllers/MessageController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -54,6 +55,18 @@ public class MessageController extends AbstractController {
 		result.addObject("messages", messages);
 		return result;
 	}
+	
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public ModelAndView edit(@RequestParam final int messageId) {
+		ModelAndView result;
+		Message message;
+
+		message = messageService.findOne(messageId);
+		Assert.notNull(message);
+		result = this.createEditModelAndView(message);
+
+		return result;
+	}
 
 	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
 	public ModelAndView save(@Valid final Message message,
@@ -66,7 +79,7 @@ public class MessageController extends AbstractController {
 			try {
 				this.actorService.sendMessage(message.getRecipient(),
 						message.getSender(), message);
-				result = new ModelAndView("redirect:list.do");
+				result = new ModelAndView("redirect:../folder/list.do");
 			} catch (final Throwable oops) {
 				result = this.createEditModelAndView(message,
 						"message.commit.error");
@@ -80,7 +93,7 @@ public class MessageController extends AbstractController {
 
 		try {
 			this.actorService.deleteMessage(message.getSender(), message);
-			res = new ModelAndView("redirect:list.do");
+			res = new ModelAndView("redirect:../folder/list.do");
 		} catch (final Throwable oops) {
 			res = this.createEditModelAndView(message, "message.commit.error");
 		}

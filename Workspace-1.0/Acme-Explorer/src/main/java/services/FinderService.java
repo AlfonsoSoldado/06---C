b1ca14a959cd495bb34154276866c58649ae85fc
Alfonso Soldado Caro <32943650+AlfonsoSoldado@users.noreply.github.com>
2017-12-20 12:36:30 +0100

    Finder medio que pichi picha

--- Workspace-1.0/Acme-Explorer/src/main/java/services/FinderService.java
+++ Workspace-1.0/Acme-Explorer/src/main/java/services/FinderService.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 
+import org.joda.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import repositories.FinderRepository;
+import domain.Configuration;
 import domain.Finder;
 import domain.Trip;
 
@@ -22,7 +24,11 @@ public class FinderService {
 	@Autowired
 	private FinderRepository finderRepository;
 	
+	@Autowired
+	private ConfigurationService configurationService;
+	
 	// Constructors
+	
 	
 	public FinderService(){
 		super();
@@ -57,9 +63,47 @@ public class FinderService {
 
 	public Finder save(Finder finder) {
 		Assert.notNull(finder);
-		Finder res;
-		res = this.finderRepository.save(finder);
+
+		Finder res = null;
+
+		Configuration configuration;
+		Integer lastOne = configurationService.resId();
+
+		configuration = configurationService.findOne(lastOne);
+
+		Integer cache = configuration.getCache() * 3600000;
+
+		Integer actual = LocalDateTime.now().getMillisOfSecond();
+
+		Collection<Finder> finders = findAll();
+
+		if (finders.isEmpty()) {
+			res = this.finderRepository.save(finder);
+		} else {
+			for (Finder f : finders) {
+				Long diff = actual - f.getCache().getTime();
+
+				if (diff > cache) {
+					delete(f);
+					res = this.finderRepository.save(finder);
+				} else if (f.getSingleKey().equals(finder.getSingleKey())
+						&& diff < cache) {
+					res = findOne(f.getId());
+				}
+			}
+			if(res == null){
+				res = this.finderRepository.save(finder);
+			}
+		}
+		
 		return res;
+	}
+	
+	public void delete(Finder finder) {
+		Assert.notNull(finder);
+		Assert.isTrue(finder.getId() != 0);
+		Assert.isTrue(this.finderRepository.exists(finder.getId()));
+		this.finderRepository.delete(finder);
 	}
 
 	// Other business methods
@@ -75,6 +119,17 @@ public class FinderService {
 	public Collection<Trip> findSearchSingleKey(String singleKey) {
 		Collection<Trip> res = new ArrayList<Trip>();
 		res.addAll(finderRepository.resultFinder(singleKey));
+//		Collection<Finder> finders = findAll();
+//		
+//		for (Finder f : finders) {
+//			if (f.getSingleKey().equals(singleKey)) {
+//				res.addAll(f.getTrip());
+//			} else {
+//				res.addAll(finderRepository.resultFinder(singleKey));
+//			}
+//
+//		}
+		
 		return res;
 	}
 	
